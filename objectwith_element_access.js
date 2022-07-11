3ea10bc46ef2274dff0ee6533172a0e7846fd3ea
sachin <abc@gmail.com>
2022-07-11 08:04:08 +0530

    new prorans

---
+++ objectwith_element_access.js
@@ -0,0 +1,13 @@
+
+var obj = {
+  '0' :'sachin' ,
+  '1':32,
+  name:'hi'   
+};
+
+
+// ------------------------
+
+console.log(obj['1']); // because obj.3 is invalid bz 3 is not valid key (data type variable)
+
+console.log(obj['name']);
