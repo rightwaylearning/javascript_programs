c17c7dd1e3ded988e4b47d312f5713d972780a08
Sahil <Sahil@gmail.com>
2022-07-21 11:14:08 +0530

    my fast program

---
+++ sahil/test.js
@@ -0,0 +1,53 @@
+//it is the 8 primitve data type js 
+// number
+// bigint
+// string
+// object
+// symbol
+// boolean
+// null 
+// undefined
+
+let b = 10;
+ console.log(typeof b);
+ 
+ let a = 3746587366348756783475638475643785;
+console.log(a);
+console.log(typeof a);
+
+//---------------------------//
+ 
+let n = 3746587366348756783475638475643785n;
+ console.log (typeof n);
+console.log (n);
+
+//---------------------------//
+
+let str1 = 'hello good morning my fast program is run';
+let str2 = "hello good morning my 'fast' program is run ";
+console.log (str1);
+console.log (str2);
+console.log ( typeof str1);
+
+//---------------------------//
+
+let sah = "sahil";
+let str3 = `hello good morning ${sah}`;
+let str4 = "hello good moring ${sah}";
+console.log (str3);
+console.log (str4);
+console.log (typeof str3);
+
+//-------------------------//
+
+let obj = {
+
+    name : "sahil",
+    age :25,
+    stu : 8,
+    status : false
+
+};
+console.log (obj);
+
+//-----------*************---------//
