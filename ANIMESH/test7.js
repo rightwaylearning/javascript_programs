763bd71676578c6921d1d0fd157035dee6ae6a3e
Animesh508 <animeshpatil43@gmail.com>
2022-07-21 16:50:24 +0530

    second day

---
+++ ANIMESH/test7.js
@@ -0,0 +1,27 @@
+var a = 10;
+
+function hello(){
+    console.log("Good morning sir....");
+}
+
+a = hello;
+a();
+
+var c = function calltype(){
+    console.log("good morning this is animesh");
+}
+
+c();
+
+ var d = function type(){
+    console.log("use this type to call for function");
+}
+
+function cat(){
+var j = function d(){
+
+}
+
+}
+
+
