c5007d61a707d8c3fd7d6e544b2bd2339b48fcc1
sachin <abc@gmail.com>
2022-08-02 08:01:49 +0530

    type convertor

---
+++ type_convertion/data_type_convertor_examples.js
@@ -0,0 +1,66 @@
+
+let r = "9"/3;
+console.log(r);
+
+let n = Number("9");
+console.log(typeof n);
+
+//----------------------------------
+
+let n1 = Number("abc");
+console.log(n1);  // NaN
+console.log(typeof n1);
+
+//-----------------------------------
+
+// 1 mean >>  true   >> 1
+// 0 means  >> false  >> 0
+
+let b1 = Number(false);
+console.log(b1);
+
+
+//>>>>>>>>
+
+let n2 = Number("");
+console.log(">>>>>>>>>>>>>>>>>>>>>>>"+n2);  // NaN
+
+
+// Javascript
+// javascript
+
+let logNum = 35453452462734253645325n;
+
+console.log(Number(logNum));
+
+
+//---------------------------
+// let pwd = Symbol("skey");
+// let num =Number(pwd);
+
+//console.log(">>>>>>>>>>>>>>>>"+num);
+
+// String();
+let obj = {key:10,
+           key2:20}
+
+console.log(String(obj));  // "unedfuned"
+
+// Boolean
+
+
+console.log(">>>>>>>>"+Boolean({}));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
